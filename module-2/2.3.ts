8c3f7e2a1b2b0d40279ed26edf4680146ad0cb51
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-05-11 15:10:32 -0700

    first commit

---
+++ module-2/2.3.ts
@@ -0,0 +1,25 @@
+{
+    //
+    type GenericArray<T>=Array<T>
+    // const rollNumber: number[] = [3, 6, 8];
+    const rollNumber: GenericArray<number> = [3, 6, 8];
+    const mentors: GenericArray<string> = ['Mukta', 'Sudipto', 'Protima']
+    const boolArray: GenericArray<boolean> = [true, false, true];
+
+    const user:GenericArray<{name:string,age:number}> = [
+        {
+            name: "Mukta",
+            age:100
+        },
+        {
+            name: "Sudipto",
+            age:1
+        }
+    ]
+
+
+    //generic tuple
+    type GenericTuple<X,Y> = [X,Y]
+    const manush:GenericTuple<string,string> =['Sudipto', 'Mukta']
+    //
+}
