bdcd153c89adb260f594e9ac276434f6f1bfc99a
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-05-15 02:42:34 -0700

    asynchronous typescript

--- module-2/2.8.ts
+++ module-2/2.8.ts
@@ -1,18 +1,40 @@
-const createPromise = () => {
-    return new Promise((resolve, reject)=> {
-    const data: string = null;
-    if (data) {
-        resolve(data);
+{
+
+    type Todo = {
+        id: number;
+        userId: number;
+        title: string;
+        completed: boolean;
     }
-    else {
-        reject("failed to load data")
-    }
+
+    //promise
+    const getTodo = async():Promise<Todo> => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+        const data = await response.json();
+        return data
+        // console.log(data);
+    };
+    getTodo();
+    type Something = { something: string };
+
+    // simulate
+    const createPromise = (): Promise<Something> => {
+        return new Promise<Something>((resolve, reject) => {
+            const data: Something = { something: "something" };
+            if (data) {
+                resolve(data);
+            }
+            else {
+                reject("failed to load data")
+            }
     
-    })
+        })
+    }
+    // calling create promise function
+    const showData = async (): Promise<Something> => {
+        const data: Something = await createPromise();
+        return data
+        // console.log(data);
+    }
+    showData();
 }
-// calling create promise function
-const showData = async () => {
-    const data = await createPromise();
-    console.log(data);
-}
-showData();
